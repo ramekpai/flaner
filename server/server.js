908019241bdcfa905879017c35ace6ad95f8aa0d
finalyfin <finalyfin@MacBook-Air-Tatarintsev.local>
2023-04-01 23:40:25 +0500

    добавил сервер и маршруты

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const fastify = require('fastify')({
+  logger: true
+})
+
+fastify.register(require('./route'))
+
+fastify.listen({ port: 3000 }, function (err, address) {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+  // Server is now listening on ${address}
+})
